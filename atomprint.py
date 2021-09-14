795d8411c8a95147f098dd5d05ab450dced291bb
nickjarvis <90723789+nickjarvis@users.noreply.github.com>
2021-09-14 16:48:51 -0400

    Add files via upload

---
+++ atomprint.py
@@ -0,0 +1,2 @@
+name = input ('whats your name?\n')
+print 'asome (name)'
---
+++ tempconvert.py
@@ -0,0 +1,3 @@
+cel = input ('whats the Celsius temperature you want converted? \n')
+fer= int(cel) * 1.8 + 32
+print (fer)
